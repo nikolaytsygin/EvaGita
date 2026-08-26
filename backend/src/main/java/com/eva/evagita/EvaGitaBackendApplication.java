5ef53b254176e0074802766862ad9c283e5ef563
oolonnad <oolonnad@student21-school.ru>
2026-08-26 12:25:19 +0300

    chore: start EvaGita project

---
+++ backend/src/main/java/com/eva/evagita/EvaGitaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eva.evagita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaGitaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaGitaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eva/evagita/EvaGitaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eva.evagita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaGitaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
